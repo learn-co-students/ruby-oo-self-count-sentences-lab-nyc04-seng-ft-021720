963f1f8874d08689bfed8d3b23885a686ccf6b7a
Kevin Clark <kevin.clark93@gmail.com>
2020-02-20 16:41:53 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # can split by spaces and use the end_with methods above to check if each element is a statement?
+    
+    word_ar = self.split(" ")
+    sentences = []
+    word_ar.each{|word|
+      if word.sentence? || word.question? || word.exclamation?
+        sentences << word
+      end
+      }
+    sentences.count
   end
 end
