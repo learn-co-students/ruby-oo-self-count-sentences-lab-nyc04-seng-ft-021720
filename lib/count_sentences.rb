6342e105329804c2008f7d5c3e7c80996d189be5
Sir Eric <echeungsk@gmail.com>
2020-02-20 18:32:17 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      TRUE
+    else
+      FALSE
+    end
   end
 
   def question?
-
+     if self.end_with?("?")
+      TRUE
+    else
+      FALSE
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      TRUE
+    else
+      FALSE
+    end
   end
 
   def count_sentences
-
+    @count_list = self.split(/[!?.]/)
+    @count = @count_list.select{|string| string.length>1}
+    @count.length
+    
   end
+  
+  
 end
